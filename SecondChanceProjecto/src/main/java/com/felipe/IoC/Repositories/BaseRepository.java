498b1b304ec5c10f4abfc3c4b8d2ea64cf57254b
Janesarai <saraipalaciosjane@gmail.com>
2022-12-28 16:53:14 -0600

    jane repo cambio
    cambie lo ultimo de repo publi

--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/BaseRepository.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/BaseRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BaseRepository<T> extends CrudRepository <T, Long> {
     List<T> findAll();
 }
