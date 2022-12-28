5a9d41c939c869479d45980de98a09dce9e048d3
Janesarai <saraipalaciosjane@gmail.com>
2022-12-28 17:27:47 -0600

    noebea jane
    cambie eso chiquito

--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/BaseRepository.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/BaseRepository.java
@@ -1,10 +1,11 @@
 package com.felipe.IoC.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface BaseRepository<T> extends CrudRepository <T, Long> {
     List<T> findAll();
 }
