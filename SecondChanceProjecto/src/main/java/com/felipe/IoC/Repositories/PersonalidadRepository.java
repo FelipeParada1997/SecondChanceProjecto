dc77d37e7e402c6549da2154da049e6e25e29b20
Hernandez2003 <115019471+Hernandez2003@users.noreply.github.com>
2022-12-29 18:18:21 -0300

    Repositorios
    Se crearon los respectivos repositorios, solo faltan agregarles los modelos para ya finalizar.

---
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/PersonalidadRepository.java
@@ -0,0 +1,8 @@
+package com.felipe.IoC.Repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonalidadRepository extends BaseRepository<T>{
+
+}
---
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/UserInstRepository.java
@@ -0,0 +1,10 @@
+package com.felipe.IoC.Repositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.felipe.IoC.Models.Animales;
+
+@Repository
+public interface UserInstRepository extends BaseRepository<UserInst>{
+
+}
