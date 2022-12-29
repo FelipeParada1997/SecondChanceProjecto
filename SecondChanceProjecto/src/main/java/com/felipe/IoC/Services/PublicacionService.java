0556f0261f7a080789f31d258df1dddb7188e5b4
Janesarai <saraipalaciosjane@gmail.com>
2022-12-28 19:40:33 -0600

    pull a mi rama jane
    cambie cosas

--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Services/PublicacionService.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Services/PublicacionService.java
@@ -1,8 +1,16 @@
 package com.felipe.IoC.Services;
 
+<<<<<<< Updated upstream
+=======
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+>>>>>>> Stashed changes
 import com.felipe.IoC.Models.Publicacion;
 import com.felipe.IoC.Repositories.PublicacionRepository;
 
+@Service
 public class PublicacionService extends BaseService<Publicacion>{
     public PublicacionService (PublicacionRepository baseRepository){
         super(baseRepository);
