1905f074f7b5681b985fe8b3d44aacfcbbdc382c
Janesarai <saraipalaciosjane@gmail.com>
2022-12-29 01:02:29 -0600

    mas cambio pequeño
    cambio pequeño

--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Controllers/UserController.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Controllers/UserController.java
@@ -1,22 +1,17 @@
 package com.felipe.IoC.Controllers;
 
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.felipe.IoC.Models.Publicacion;
 import com.felipe.IoC.Models.User;
 import com.felipe.IoC.Services.PublicacionService;
 import com.felipe.IoC.Services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -31,22 +26,27 @@ public class UserController {
 
     //---------------------------------------------register login--------------------------------------------------
 
+    @RequestMapping(value = "/", method= RequestMethod.GET)
+    public String index(Model model, HttpSession session,
+                        @ModelAttribute("user") User user) {
+        return "/user/index.jsp";
+    }
+
     @PostMapping("/registro")
     public String indexregister(@Valid @ModelAttribute("user") User user,
                                 BindingResult result, HttpSession session, Model model) {
         if (result.hasErrors()) {
-            return "registro";
+            return "/user/index";
+        }
+        if(!user.getPassword().equals(user.getPasswordConfirm())) {
+            model.addAttribute("Error", "Las contraseñas no son iguales");
+            return "/user/index.jsp";
         }
         User u = userService.registerUser(user);
         session.setAttribute("userId", u.getId());
         return "redirect:/home";
+
     }
-    
-    @GetMapping("/registro")
-    public String vistaRegistro(@ModelAttribute("user")User user){
-        return "registro";
-    }
-    
 
     @PostMapping("/login")
     public String indexlogin(@RequestParam("email") String email,
@@ -58,7 +58,7 @@ public class UserController {
             return "redirect:/home";
         } else {
             model.addAttribute("error","Please try again");
-            return "login";
+            return "/user/index";
         }
     }
 
@@ -69,24 +69,16 @@ public class UserController {
 
     //---------------------------------------------Home--------------------------------------------------
 
-// @GetMapping("/home")
-//     public String home(Model model,HttpSession session) {
-//         if((Long) session.getAttribute("userId") == null) {
-//             return "redirect:/";
-//         }else {
-//             List<Publicacion> publicaciones = publicacionService.mostrarPublicacions();
-//             model.addAttribute("listpulicacion", publicaciones);
-//             model.addAttribute("user", userService.findUserById((Long)session.getAttribute("userId")));
-//             return "home";
-//         }
-//     }
-
-//para ver publicaciones en el home en general
-    @GetMapping("home")
-    public String home(@ModelAttribute ("user")User user, Model model){
-        List<Publicacion> publicaciones = publicacionService.mostrarPublicacions();
-        model.addAttribute("publicaciones", publicaciones);
-    return"home";
+    @RequestMapping (value = "/home", method = RequestMethod.GET)
+    public String home(Model model,HttpSession session) {
+        if((Long) session.getAttribute("userId") == null) {
+            return "redirect:/";
+        }else {
+            List<Publicacion> publicaciones = publicacionService.mostrarPublicacions();
+            model.addAttribute("listpulicacion", publicaciones);
+            model.addAttribute("user", userService.findUserById((Long)session.getAttribute("userId")));
+            return "/user/home";
+        }
     }
 
 }
