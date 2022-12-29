1bad8a20ca428445aaab0a5889b90afdfe8826cc
Janesarai <saraipalaciosjane@gmail.com>
2022-12-28 18:04:40 -0600

    publicacion controller
    keke

--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Controllers/PublicacionController.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Controllers/PublicacionController.java
@@ -1,4 +1,17 @@
 package com.felipe.IoC.Controllers;
 
+import org.springframework.stereotype.Controller;
+
+import com.felipe.IoC.Services.PublicacionService;
+import com.felipe.IoC.Services.UserService;
+
+@Controller
 public class PublicacionController {
+
+    private final PublicacionService publicacionService;
+    private final UserService userService;
+
+    public PublicacionController(PublicacionService publicacionService,UserService userService){
+        this.
+    }
 }
