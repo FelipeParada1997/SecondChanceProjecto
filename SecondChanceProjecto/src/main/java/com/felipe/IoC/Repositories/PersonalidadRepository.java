45f77f882c60c88bc57a0a12320de54d3fd63ace
Hernandez2003 <115019471+Hernandez2003@users.noreply.github.com>
2022-12-30 03:21:14 -0300

    Se arreglaron todos los repositorios
    se borro el likerepository

--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/PersonalidadRepository.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/PersonalidadRepository.java
@@ -2,7 +2,9 @@ package com.felipe.IoC.Repositories;
 
 import org.springframework.stereotype.Repository;
 
+import com.felipe.IoC.Models.Personalidad;
+
 @Repository
-public interface PersonalidadRepository extends BaseRepository<T>{
+public interface PersonalidadRepository extends BaseRepository<Personalidad>{
 
 }
--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/VacunaRepository.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/VacunaRepository.java
@@ -2,7 +2,9 @@ package com.felipe.IoC.Repositories;
 
 import org.springframework.stereotype.Repository;
 
+import com.felipe.IoC.Models.Vacuna;
+
 @Repository
-public interface VacunaRepository extends BaseRepository<T>{
+public interface VacunaRepository extends BaseRepository<Vacuna>{
 
 }
