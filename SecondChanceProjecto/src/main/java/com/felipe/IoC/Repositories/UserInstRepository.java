2e7e9639b6ad417ace61c40b8bb1d46cd2fa42a6
Hernandez2003 <115019471+Hernandez2003@users.noreply.github.com>
2022-12-30 03:34:16 -0300

    Update UserInstRepository.java

--- SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/UserInstRepository.java
+++ SecondChanceProjecto/src/main/java/com/felipe/IoC/Repositories/UserInstRepository.java
@@ -2,9 +2,10 @@ package com.felipe.IoC.Repositories;
 
 import org.springframework.stereotype.Repository;
 
+import com.felipe.IoC.Models.User;
 import com.felipe.IoC.Models.UserInst;
 
 @Repository
 public interface UserInstRepository extends BaseRepository<UserInst>{
-
+	User findByEmail(String email);
 }
